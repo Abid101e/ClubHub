d24ff8d2ed8eff446a22056f41c2e8a69d13ff2d
Md. Abid Hasan <emonff89@gmail.com>
2025-12-31 17:01:08 +0600

    implement user authentication with signup, login, and logout; add base template and club listing page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from accounts.forms import CustomUserCreationForm
 
-# Create your views here.
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('clubs:list')
+    template_name = 'accounts/signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
